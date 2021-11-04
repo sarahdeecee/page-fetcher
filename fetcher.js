dcfc81fe29e41f2e8d90d3a817c6419753db0cf1
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-03 22:23:27 -0400

    implemented page-fetcher which clones an HTML page

---
+++ fetcher.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const fs = require('fs');
+
+const fetcher = (url, path) => {
+  request(url, (error, response, body) => {
+    (error) ? console.log('error:', error): null;
+    content = body;
+    fs.writeFile(path, body, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      fileWritten(content);
+    })
+  });
+}
+
+const calcBytes = data => {
+  let size = data.length;
+  return size;
+}
+
+const fileWritten = (data) => {
+  let size = calcBytes(data);
+  console.log(`Downloaded and saved ${size} bytes to ${path}`);
+}
+
+const inputs = process.argv.slice(2);
+let url = inputs[0]
+let path = inputs[1];
+fetcher(url, path);
