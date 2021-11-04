0b921385631dc4e7ff4eb5b9013bbb96824edcd9
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-03 22:26:31 -0400

    cleaned up code

--- fetcher.js
+++ fetcher.js
@@ -3,29 +3,28 @@ const fs = require('fs');
 
 const fetcher = (url, path) => {
   request(url, (error, response, body) => {
-    (error) ? console.log('error:', error): null;
-    content = body;
+    (error) ? console.log('error:', error) : null;
     fs.writeFile(path, body, err => {
       if (err) {
         console.error(err);
         return;
       }
-      fileWritten(content);
-    })
+      fileWritten(body);
+    });
   });
-}
+};
 
 const calcBytes = data => {
   let size = data.length;
   return size;
-}
+};
 
 const fileWritten = (data) => {
   let size = calcBytes(data);
   console.log(`Downloaded and saved ${size} bytes to ${path}`);
-}
+};
 
 const inputs = process.argv.slice(2);
-let url = inputs[0]
+let url = inputs[0];
 let path = inputs[1];
 fetcher(url, path);
